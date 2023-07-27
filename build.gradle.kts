b192c1a346118bcf3d2ffa6494a719bb58a5fd4d
Alexey <alexey.merov@gmail.com>
2023-07-27 19:35:10 +0300

    - removed legacy plugin init

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,18 +1,10 @@
 import com.github.benmanes.gradle.versions.updates.DependencyUpdatesTask
 
-buildscript {
-    dependencies {
-        classpath(libs.plugin.gradle)
-        classpath(libs.plugin.kotlin)
-        classpath(libs.plugin.hilt)
-        classpath(libs.plugin.versionsChecker)
-    }
-}
-
 plugins {
     alias(libs.plugins.androidApplication) apply false
     alias(libs.plugins.androidLibrary) apply false
     alias(libs.plugins.kotlinAndroid) apply false
+    alias(libs.plugins.hilt) apply false
     alias(libs.plugins.ksp) apply false
     alias(libs.plugins.ktlint)
     alias(libs.plugins.versionsChecker)
