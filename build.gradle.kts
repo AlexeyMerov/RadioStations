d52abb81abff5355a5a615cd49c030f5e2ce18cd
Alex <AlexeyMerov@users.noreply.github.com>
2023-12-13 18:21:33 +0200

    - fix ci ktlint

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
     alias(libs.plugins.kotlinAndroid) apply false
     alias(libs.plugins.hilt) apply false
     alias(libs.plugins.ksp) apply false
-    alias(libs.plugins.ktlint) apply false
+    alias(libs.plugins.ktlint)
     alias(libs.plugins.versionsChecker) apply false
     alias(libs.plugins.googleServices) apply false
     alias(libs.plugins.firebase.crashlytics) apply false
