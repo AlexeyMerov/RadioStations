c207a0c281627e52127c58ba650d1d5e09417373
Alex <AlexeyMerov@users.noreply.github.com>
2024-03-02 15:58:33 +0200

    - change deps for test libs

--- core/test/build.gradle.kts
+++ core/test/build.gradle.kts
@@ -8,21 +8,19 @@ android {
 }
 
 dependencies {
-    api(libs.junit)
-    api(libs.junitExt)
-
     api(libs.test.core)
     api(libs.test.coreKtx)
     api(libs.test.runner)
 
-    api(libs.google.truth)
+    api(libs.test.ext.junit)
+    api(libs.test.ext.junitExt)
+    api(libs.test.ext.truth)
 
     api(libs.mockk.android)
     api(libs.mockk.agent)
 
     api(libs.coroutines.test)
-
-    api(libs.hilt.testing)
-
     api(libs.paging.runtime)
+
+    implementation(libs.hilt.testing)
 }
